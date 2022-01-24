9d1fd82e3da5cc8a10d25e3e29e631b6650c2ad5
Leila-Bel <leilabelahcen@outlook.com>
2022-01-24 17:43:18 -0500

    added Ahmad's data files and read them in test.py part 2

--- Code/Test.py
+++ Code/Test.py
@@ -1,4 +1,24 @@
-#Hello
 
-print("Hello")
-print("Another Test")
+from Sensor_Data_Reader import *
+
+#finger up file
+read_file('../DataSet/Finger_up.csv')
+
+#finger down
+read_file('../DataSet/Finger_down.csv')
+
+#finger left
+read_file('../DataSet/Finger_Left.csv')
+
+#finger right
+read_file('../DataSet/Finger_right.csv')
+
+#90 deg clock
+read_file('../DataSet/90deg_clockwise.csv')
+
+#90 deg counter
+read_file('../DataSet/90deg_counter_clockwise.csv')
+
+
+
+
