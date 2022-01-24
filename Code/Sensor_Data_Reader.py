6b0dc91c1595d43faf4051e9f53bf266bb9a35e2
Jacob <jacobrodolfo1999@gmail.com>
2022-01-24 18:08:07 -0500

    Fixed Warning, set low_memory=False

--- Code/Sensor_Data_Reader.py
+++ Code/Sensor_Data_Reader.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def read_file(data_file):
-    d_file = pd.read_csv(data_file,
+    d_file = pd.read_csv(data_file, low_memory=False,
                          usecols=['ACC-X', 'ACC-Y', 'ACC-Z', 'GYRO-X', 'GYRO-Y', 'GYRO-Z', 'TEST'])
     print(d_file)
     return d_file.dropna()
