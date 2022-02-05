a64aa9195fbb470bbc7a1a92b0fdf1fd22e001bc
Leila-Bel <leilabelahcen@outlook.com>
2022-02-05 00:10:21 -0500

    #3 clockwise file now works

--- Code/Data_Frame_Merging.py
+++ Code/Data_Frame_Merging.py
@@ -1,8 +1,7 @@
 import pandas as pd
-import os
 
 
-def dataframe_merging(data_frame1, data_frame2, data_frame3, data_frame4, data_frame5):
-    frames = [data_frame1, data_frame2, data_frame3, data_frame4, data_frame5]
+def dataframe_merging(data_frame1, data_frame2, data_frame3, data_frame4, data_frame5, data_frame6):
+    frames = [data_frame1, data_frame2, data_frame3, data_frame4, data_frame5, data_frame6]
     result = pd.concat(frames)
     return result
--- Code/Feature_Extraction.py
+++ Code/Feature_Extraction.py
@@ -1,25 +1,11 @@
-import csv
-
 import pandas as pd
-import numpy as np
-import seaborn as sns
-
-from statsmodels.tsa.stattools import acovf
 
 
 def feature_extraction(data_file, test_type, ring_number):
-    # TODO:min, max, mean skewness, kurtosis, variance, autocovariance(?)
+    # TODO: autocovariance(?), maybe fix warnings
 
     features = pd.DataFrame(data_file)
     grouped = features.groupby(features["TEST"])
-
-    # gesture_df = pd.DataFrame()
-    # def test_type():
-    #     test = pd.DataFrame()
-    #
-    #     return test
-        # for group in range(grouped.ngroups - 1):
-        #     test = test.
 
     def minimum(data_group):
         min_df = data_group.min().to_frame().transpose()
@@ -122,70 +108,9 @@ def feature_extraction(data_file, test_type, ring_number):
             # max_df = pd.concat()
         return kurt_df
 
-    # print(min_df())
-    # print(max_df())
     c_df = min_df().join(max_df()).join(mean_df()).join(variance_df()).join(skewness_df()).join(kurtosis_df())
     c_df.insert(0, 'GESTURE', test_type)
+
     # print(c_df)
-    # print(test_type())
-    # c_df.to_csv('../DataSet/extracted_features.csv', mode='a', index=False)
-
-    # for group in range(grouped.ngroups - 1):
-    #     df = grouped.get_group(str(group + 1)).drop(columns="TEST")
-    #     print(minimum(df))
-
-    # kurt.to_csv('../DataSet/extracted_features.csv', mode='a', index=False, header=False)
 
     return c_df
-
-    # file_test.to_csv('../DataSet/extracted_features_trial.csv',mode='a', header=["ACC-X", "ACC-Y", "ACC-Z", "GYRO-X", "GYRO-Y", "GYRO-Z"], index=False)
-
-    # print(kurt)
-    # writer.writerow([kurt])
-
-    # print(df.kurtosis())
-
-    # feature_file.close()
-
-    # kurt_values = kurt.iloc[0:, :]
-    # header = ["ACC-X", "ACC-Y", "ACC-Z", "GYRO-X", "GYRO-Y", "GYRO-Z"]
-    # kurt.DictWriter()
-
-    # test_column = grouped.head()
-    # print("Start Test")
-    # print(grouped.get_group("5"))
-    # print("End Test")
-
-    # feature_file = open('../DataSet/extracted_features.csv', 'w')
-    # writer = csv.writer(feature_file)
-
-
-
-    # grouped = data_file.groupby(data_file.TEST)
-    # print(grouped.ngroups)
-    # df5 = grouped.get_group("2000")
-    # print(df5)
-
-    # for value in test_num:
-    #     if str(value) == str(test_num):
-    #         separate_test_instances = pd.DataFrame(columns=data_file.columns, dtype=np.float64)
-    #         print(separate_test_instances)
-
-    # #create data structure
-    # features = pd.DataFrame(columns=data_file.columns, dtype=np.float64)
-    #
-    # def append_features(label, data_file):
-    #     def ordering(col_values):
-    #         return col_values[features.columns].values
-    #
-    #     #check label and store associated values
-    #     features.loc[label, :] = ordering(data_file)
-    #
-    # append_features('min', data_file.min())
-    # append_features('max', data_file.max())
-    # append_features('mean', data_file.mean())
-    # append_features('var', data_file.var(ddof=0))
-    # append_features('skew', data_file.skew())
-    # append_features('kurtosis', data_file.kurtosis())
-    #
-    # return features
